e7b43e4bc2ef72852d613e9447c3c760419003ce
Tammo Ippen <tammo.ippen@posteo.de>
2020-09-11 17:04:43 +0200

    Fix over/underflow with numpy.int16 (#36)
    * Fix under/overflow with numpy int16
    * v3.7.2

---
+++ tests/test_util.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+
+from plotille._util import hist
+
+
+def test_hist_neg_idx():
+    x = np.random.randint(-32767, 32767, 100, dtype=np.int16)
+
+    expect = np.histogram(x, bins=8)
+    actual = hist(x, bins=8)
+
+    assert list(expect[0]) == actual[0]  # counts
+    assert list(expect[1]) == actual[1]  # bins
