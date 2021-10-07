eac145d4f9ee0e1ff69fa65aaccb897c3f95197d
Nik Tezak <nik@openai.com>
2021-10-07 17:01:50 -0600

    Add setup.py for pip-installability

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="plotille",
+    version = "3.9.0",
+    packages=find_packages(),
+    description = "Plot in the terminal using braille dots.",
+    authors = ["Tammo Ippen <tammo.ippen@posteo.de>"],
+    license = "MIT",
+)
