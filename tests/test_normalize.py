5b4cf9cadb8b2094b5994819d56d42354d94ded4
Tammo Ippen <tammo.ippen@posteo.de>
2021-10-20 21:50:00 +0200

    tests for normalize

---
+++ tests/test_normalize.py
@@ -0,0 +1,42 @@
+import math
+from random import random
+
+from plotille._normalize import Normalize
+
+
+def test_random_numbers():
+    norm = Normalize(-10, 10)
+
+    for _ in range(1000):
+        x = random() * 20 - 10
+        norm_x = norm(x)
+        assert 0 <= norm(x) <= 1
+        assert x == norm_x * 20 - 10
+
+
+def test_random_array():
+    norm = Normalize(-10, 10)
+
+    for _ in range(1000):
+        x = [random() * 20 - 10 for _ in range(10)]
+        norm_x = norm(x)
+        assert len(x) == len(norm_x)
+        assert all(0 <= y <= 1 for y in norm_x)
+        assert x == [y * 20 - 10 for y in norm_x]
+
+
+def test_bad_values():
+    norm = Normalize(-10, 10)
+
+    assert math.isinf(norm(math.inf))
+    assert math.isnan(norm(math.nan))
+    assert norm(None) is None
+
+
+def test_bad_values_clip():
+    norm = Normalize(-10, 10, clip=True)
+
+    assert norm(math.inf) == 1.0
+    assert norm(math.nan) == 1.0
+    # is not a number
+    assert norm(None) is None
