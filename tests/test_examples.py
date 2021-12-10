63553c6a8f6212aaec594a85de0d4836fb8f4406
Tammo Ippen <tammo.ippen@posteo.de>
2021-12-10 10:41:39 +0100

    revert changes

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -7,8 +7,6 @@ import sys
 
 def test_examples():
     sys.path.insert(0, './examples')
-    reload(sys)  # Reload does the trick!
-    sys.setdefaultencoding('UTF8')
     for fname in glob('examples/*_example.py'):
         name = fname.split('.')[0].split('/')[-1]
         example_module = __import__(name)
