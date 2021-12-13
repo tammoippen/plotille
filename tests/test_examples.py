397485b543a70f7a34022767b750b20e36e93bc1
Tammo Ippen <tammo.ippen@posteo.de>
2021-12-13 17:33:50 +0100

    explicit explicit?

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -18,31 +18,103 @@ try:
 except ImportError:
     have_pillow = False
 
+mains = []
+
+# have explicit imports here as both __import__ and Popen made issues
+try:
+    from examples.color_example import main as color_example
+    mains += [color_example]
+except ImportError:
+    print('Ignore example: color_example')
+
+try:
+    from examples.custom_ticks_example import main as custom_ticks_example
+    mains += [custom_ticks_example]
+except ImportError:
+    print('Ignore example: custom_ticks_example')
+
+try:
+    from examples.ellipse_example import main as ellipse_example
+    mains += [ellipse_example]
+except ImportError:
+    print('Ignore example: ellipse_example')
+
+try:
+    from examples.histograms_example import main as histograms_example
+    mains += [histograms_example]
+except ImportError:
+    print('Ignore example: histograms_example')
+
+try:
+    from examples.house_example import main as house_example
+    mains += [house_example]
+except ImportError:
+    print('Ignore example: house_example')
+
+try:
+    from examples.images_example import main as images_example
+    mains += [images_example]
+except ImportError:
+    print('Ignore example: images_example')
+
+try:
+    from examples.markers_and_labels_example import main as markers_and_labels_example
+    mains += [markers_and_labels_example]
+except ImportError:
+    print('Ignore example: markers_and_labels_example')
+
+try:
+    from examples.olympic_rings_example import main as olympic_rings_example
+    mains += [olympic_rings_example]
+except ImportError:
+    print('Ignore example: olympic_rings_example')
+
+try:
+    from examples.performance_example import main as performance_example
+    mains += [performance_example]
+except ImportError:
+    print('Ignore example: performance_example')
+
+try:
+    from examples.plot_example import main as plot_example
+    mains += [plot_example]
+except ImportError:
+    print('Ignore example: plot_example')
+
+try:
+    from examples.scatter_cats_example import main as scatter_cats_example
+    mains += [scatter_cats_example]
+except ImportError:
+    print('Ignore example: scatter_cats_example')
+
+try:
+    from examples.scatter_example import main as scatter_example
+    mains += [scatter_example]
+except ImportError:
+    print('Ignore example: scatter_example')
+
+try:
+    from examples.side_by_side_example import main as side_by_side_example
+    mains += [side_by_side_example]
+except ImportError:
+    print('Ignore example: side_by_side_example')
+
+try:
+    from examples.span_example import main as span_example
+    mains += [span_example]
+except ImportError:
+    print('Ignore example: span_example')
+
+try:
+    from examples.wetterdienst_example import main as wetterdienst_example
+    mains += [wetterdienst_example]
+except ImportError:
+    print('Ignore example: wetterdienst_example')
+
 
 @pytest.mark.skipif(not have_numpy, reason='No numpy installed.')
 @pytest.mark.skipif(not have_pillow, reason='No pillow installed.')
 def test_examples():
-    # have explicit imports here as both __import__ and Popen made issues
-    from examples.color_example import main as color_example
-    from examples.custom_ticks_example import main as custom_ticks_example
-    from examples.ellipse_example import main as ellipse_example
-    from examples.histograms_example import main as histograms_example
-    from examples.house_example import main as house_example
-    from examples.images_example import main as images_example
-    from examples.markers_and_labels_example import main as markers_and_labels_example
-    from examples.olympic_rings_example import main as olympic_rings_example
-    from examples.performance_example import main as performance_example
-    from examples.plot_example import main as plot_example
-    from examples.scatter_cats_example import main as scatter_cats_example
-    from examples.scatter_example import main as scatter_example
-    from examples.side_by_side_example import main as side_by_side_example
-    from examples.span_example import main as span_example
-    from examples.wetterdienst_example import main as wetterdienst_example
-
-    mains = [color_example, custom_ticks_example, ellipse_example, histograms_example,
-             house_example, images_example, markers_and_labels_example,
-             olympic_rings_example, performance_example, plot_example, scatter_cats_example,
-             scatter_example, side_by_side_example, span_example, wetterdienst_example]
     assert len(mains) == len(glob('./examples/*_example.py'))
     for main in mains:
         main()
