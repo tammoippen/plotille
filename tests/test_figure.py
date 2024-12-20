f2f0e2373d889c4c1658983f4435f3ca1862b6b9
Tammo Ippen <tammo.ippen@posteo.de>
2024-12-20 10:12:28 +0100

    rm legacy code

--- tests/test_figure.py
+++ tests/test_figure.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function, unicode_literals
+
+
 
 import datetime as orig_datetime
 import os
 
-from mock import call
+from unittest.mock import call
 from pendulum import datetime, duration
 import pytest
 
